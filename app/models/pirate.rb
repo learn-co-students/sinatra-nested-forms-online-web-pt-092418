9f2642f9fc26292685ba9cb106b204b8ab8e02b2
Karyme Virginia <karyme@kv.codes>
2019-01-14 11:13:43 -0800

    Done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    PIRATES = []
+
+    def initialize(info)
+        @name = info[:name]
+        @weight = info[:weight]
+        @height = info[:height]
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
 end
