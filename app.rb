b9d757b645631739a7a32ea48c6fc20abde9c5b9
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-14 11:46:15 -0500

    Interim checkin

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do
+     
+      erb :new
+    end
+
+    post '/pirates' do 
+      @pirates = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+       Ships.new(details)
+      end
+      @ships = Ships.all 
+      erb :show
+    end
+
 
   end
 end
